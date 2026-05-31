554d52707bbb3dbdb6cedaff6fd6c33ee8b0b09f
Tamara Gjelevska <gjelevskatamara>
2026-06-01 00:51:54 +0200

    email on registration fix

--- backend/config/email.js
+++ backend/config/email.js
@@ -2,7 +2,8 @@ const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
-  port: parseInt(process.env.EMAIL_PORT || '587'),
+  port: parseInt(process.env.EMAIL_PORT) || 465,
+  secure: process.env.EMAIL_SECURE === 'true',
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
